4e3d99992ff3a69c95bb173f4b7f18bb98a8b68c
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-08-23 15:46:17 -0400

    Create input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+# Example with concatenation 
+name = input("What's your name? ")
+print("Hello, " + name + "! Nice to meet you.")
+
+# Example with multiplication
+age = input("How old are you? ")
+#Note that you HAVE to convert age into an int here in order to use it
+age_in_future = int(age) + 10 
+print("In 10 years, you'll be", age_in_future)
